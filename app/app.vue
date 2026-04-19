1195a41ff68f60b385a4cea1587d18d68ba480ff
Sengkea <119031980+Sengkea2022@users.noreply.github.com>
2026-04-20 00:32:21 +0700

    update theme primary color

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+
+const elementThemeStyle = computed(() => {
+  const primaryColor = appConfig.theme.primary
+
+  return {
+    '--el-color-primary': primaryColor,
+    '--el-color-primary-light-3': `color-mix(in srgb, ${primaryColor} 70%, white)`,
+    '--el-color-primary-light-5': `color-mix(in srgb, ${primaryColor} 50%, white)`,
+    '--el-color-primary-light-7': `color-mix(in srgb, ${primaryColor} 30%, white)`,
+    '--el-color-primary-light-8': `color-mix(in srgb, ${primaryColor} 20%, white)`,
+    '--el-color-primary-light-9': `color-mix(in srgb, ${primaryColor} 10%, white)`,
+    '--el-color-primary-dark-2': `color-mix(in srgb, ${primaryColor} 80%, black)`
+  }
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div :style="elementThemeStyle">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
